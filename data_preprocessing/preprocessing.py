e16a2e2cf0ebbe49caa37ad68ced9bb3e2c0d441
gsingh121 <85227339+gsingh121@users.noreply.github.com>
2021-06-14 14:38:52 +0530

    Update preprocessing.py

--- data_preprocessing/preprocessing.py
+++ data_preprocessing/preprocessing.py
@@ -8,7 +8,7 @@ class Preprocessor:
     """
         This class shall  be used to clean and transform the data before training.
 
-        Written By: iNeuron Intelligence
+        Written By: Gourav Singh
         Version: 1.0
         Revisions: None
 
@@ -25,7 +25,7 @@ class Preprocessor:
                 Output: A pandas DataFrame after removing the specified columns.
                 On Failure: Raise Exception
 
-                Written By: iNeuron Intelligence
+                Written By: Gourav Singh
                 Version: 1.0
                 Revisions: None
 
@@ -51,7 +51,7 @@ class Preprocessor:
                         Output: Returns two separate Dataframes, one containing features and the other containing Labels .
                         On Failure: Raise Exception
 
-                        Written By: iNeuron Intelligence
+                        Written By: Gourav Singh
                         Version: 1.0
                         Revisions: None
 
@@ -75,7 +75,7 @@ class Preprocessor:
                                 Output: Returns a Boolean Value. True if null values are present in the DataFrame, False if they are not present.
                                 On Failure: Raise Exception
 
-                                Written By: iNeuron Intelligence
+                                Written By: Gourav Singh
                                 Version: 1.0
                                 Revisions: None
 
@@ -107,7 +107,7 @@ class Preprocessor:
                                         Output: A Dataframe which has all the missing values imputed.
                                         On Failure: Raise Exception
 
-                                        Written By: iNeuron Intelligence
+                                        Written By: Gourav Singh
                                         Version: 1.0
                                         Revisions: None
                      """
@@ -132,7 +132,7 @@ class Preprocessor:
                                                 Output: List of the columns with standard deviation of zero
                                                 On Failure: Raise Exception
 
-                                                Written By: iNeuron Intelligence
+                                                Written By: Gourav Singh
                                                 Version: 1.0
                                                 Revisions: None
                              """
